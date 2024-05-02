81339b3b58b0db3079a6d3942762c61433db1585
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-05-02 17:04:40 -0300

    add Link component

---
+++ app/components/Link.tsx
@@ -0,0 +1,20 @@
+import NextLink from "next/link";
+import { Link as RadixLink } from "@radix-ui/themes";
+
+type Props = {
+  href: string;
+  children: React.ReactNode;
+};
+const Link = ({ href, children }: Props) => {
+  return (
+    <NextLink
+      passHref
+      legacyBehavior
+      href={href}
+    >
+      <RadixLink>{children}</RadixLink>
+    </NextLink>
+  );
+};
+
+export default Link;
