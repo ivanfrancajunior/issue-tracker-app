f7f956de700e7b3343449aa4ce9a57079487a262
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-05-02 12:24:29 -0300

    setup radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
-  )
-}
+    <div className='p-4'>
+      <Button
+        color='orange'
+        radius='full'
+      >
+        Add Issue
+      </Button>
+    </div>
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
