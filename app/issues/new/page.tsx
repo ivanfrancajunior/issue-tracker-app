49a88b2787541040cb17b57baaaadcbb040e0a16
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-05-02 13:31:12 -0300

    handle form sunmition

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,48 @@
 "use client";
-import { Button, TextField, ThemePanel } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
+import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+type IssueForm = {
+  title: string;
+  description: string;
+};
 
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
+  const router = useRouter();
   return (
-    <div className='max-w-xl space-y-3'>
+    <form
+      className='max-w-xl space-y-3'
+      onSubmit={handleSubmit(async (data) => {
+        console.log(data);
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder='Type a title' />
+        <TextField.Input
+          placeholder='Type a title'
+          {...register("title")}
+        />
       </TextField.Root>
-      <SimpleMDE placeholder='Type a description for your issue.' />
+      <Controller
+        name='description'
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE
+            placeholder='Type a description for your issue.'
+            {...field}
+          />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-      <ThemePanel />
-    </div>
+    </form>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang='en'
-      className={inter.variable}
-    >
-      <body>
+    <html lang='en'>
+      <body className={inter.variable}>
         <Theme
           appearance='light'
           accentColor='orange'
