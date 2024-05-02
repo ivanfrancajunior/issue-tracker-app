7c098bce5a641477cdb8ec9787f2aec41f1e301a
Ivan Junior <contato.ivanfrancajr@gmail.com>
2024-05-02 12:58:23 -0300

    customize radixUi Theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextArea, TextField, ThemePanel } from "@radix-ui/themes";
 import React from "react";
 
 const NewIssuePage = () => {
@@ -10,6 +10,7 @@ const NewIssuePage = () => {
       </TextField.Root>
       <TextArea placeholder='Type a description' />
       <Button>Submit New Issue</Button>
+      <ThemePanel />
     </div>
   );
 };
